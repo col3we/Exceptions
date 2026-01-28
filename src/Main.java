7f269e855a11a8f04905dede657a5af417b2fe96
Fedor <fedormakarov634@gmail.com>
2026-01-28 13:35:58 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        String login = "user123";
+        String password = "qwerty_1";
+        String confirmPassword = "qwerty_1";
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        try {
+            UserValidation.validateCredentials(login, password, confirmPassword);
+            System.out.println("Данные прошли валидацию");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println("Ошибка: " + e.getMessage());
+        }
+    }
 }
